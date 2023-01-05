360d54ba64bc1c3be3663cafcbf6cee883235d20
RafaelDomingues <rafael.dominguesr2d2@gmail.com>
2023-01-05 15:09:25 -0300

    added theme cores and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
   return (
-    <h1>Clear</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>clear</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-export default App
