6c482d6fbaa587c64d8a48984d898c23e376566e
RafaelDomingues <rafael.dominguesr2d2@gmail.com>
2023-01-08 15:36:43 -0300

    Header completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import { HeaderContainer } from "./styled";
 
+import logo from "../../assets/Logo.svg"
+import { MapPin, ShoppingCart } from "phosphor-react";
+
 export function Header(){
     return (
         <HeaderContainer>
-            <h1>Header</h1>
+            <img src={logo} alt="Copo de café" />
+            <nav>
+                <a><MapPin size={23} weight="fill" /> Porto Alegre, RS</a>
+                <a><ShoppingCart size={23} weight="fill"/></a>
+            </nav>
         </HeaderContainer>
     )
 }
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,5 +1,40 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-    background: red;
+    display: flex;
+    justify-content: space-between;
+
+    nav {
+        display: flex;
+        align-items: center;
+
+        a:first-child {
+            background-color: ${props => props.theme["purple-light"]};
+
+            display: flex;
+            align-items: center;
+            padding: 0.5rem;
+            border-radius: 8px;
+            margin-right: -0.5rem;
+
+            svg {
+                color: ${props => props.theme["purple"]};
+                margin: 0 0.2rem;
+            }
+        }
+
+        a:last-child {
+            background-color: ${props => props.theme["yellow-light"]};
+            
+            padding: 0.4rem;
+            border-radius: 8px;
+
+            svg {
+                color: ${props => props.theme["yellow-dark"]};
+                margin: 0;
+            }
+        }
+
+      
+    }
 `
